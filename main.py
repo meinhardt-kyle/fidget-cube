fc629974161d5d30166977597776beab09da40cc
meinhardt-kyle <58998103+meinhardt-kyle@users.noreply.github.com>
2021-02-05 09:54:09 -0600

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+# unknown expression:  178
+completed_patterns = {
+    "button" : False,
+    "pins" : False,
+}
+
+while not completed_patterns['button'] and not completed_patterns['pins']:
+    pass
---
+++ main.ts
@@ -0,0 +1,9 @@
+//  unknown expression:  178
+let completed_patterns = {
+    "button" : false,
+    "pins" : false,
+}
+
+while (!completed_patterns["button"] && !completed_patterns["pins"]) {
+    
+}
