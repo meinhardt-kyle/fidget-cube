d1453cac8b881c17f0494220aa3471acf9cc45d4
meinhardt-kyle <58998103+meinhardt-kyle@users.noreply.github.com>
2021-02-05 09:59:11 -0600

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 completed_sequences = {
     "button" : False,
     "pin" : False,
+    "acceleration" : False,
+    "light" : False,
 }
 def check_for_completion():
-    return True if completed_sequences['button'] and completed_sequences['pin'] else False
+    return True if completed_sequences['button'] and completed_sequences['pin'] and completed_sequences['acceleration'] and completed_sequences["light"] else False
 
 while not check_for_completion():
     pass
--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@
 let completed_sequences = {
     "button" : false,
     "pin" : false,
+    "acceleration" : false,
+    "light" : false,
 }
 
 function check_for_completion() {
-    return completed_sequences["button"] && completed_sequences["pin"] ? true : false
+    return completed_sequences["button"] && completed_sequences["pin"] && completed_sequences["acceleration"] && completed_sequences["light"] ? true : false
 }
 
 while (!check_for_completion()) {
